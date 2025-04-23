e4c4521a9a7ec42d454955532b36dd9adb55b2bd
tazeen04 <atazeenm@gmail.com>
2025-04-23 12:03:41 +0530

    add typescript

--- src/server.js
+++
@@ -1,2 +0,0 @@
-console.log("Welcome to FullStack JavaScript!");
-console.log("This is a simple server setup.");
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+
+function welcome(name: string) {
+  console.log(`Welcome, ${name}!`);
+}
+
+welcome("John Doe");
