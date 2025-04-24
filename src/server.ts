47739dcb1f7b747a25a3314280e91868d204a62a
tazeen04 <atazeenm@gmail.com>
2025-04-25 02:24:56 +0530

    test linkStaged 2

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 function welcome(name: string) {
-    console.log(`Welcome, ${name}!`)
+    console.log(`Welcome, ${name}!`);
+
+    const user = {
+        name: 'ameena',
+    };
+
+    const fname = user.name;
+
+    return name + fname;
 }
 
-welcome('John Doe')
+welcome('John Doe');
