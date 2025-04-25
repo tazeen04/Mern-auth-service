68e91d4a69950e65070ef4be86431c84c76cf909
tazeen04 <atazeenm@gmail.com>
2025-04-25 22:27:15 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { Config } from './config';
 import app from './app';
+import logger from './config/logger';
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
-        app.listen(PORT, () => console.log(`Listening on pory ${PORT}`));
+        app.listen(PORT, () => {
+            logger.info('Listening on port', { port: PORT });
+        });
     } catch (err) {
         console.error('Error starting server:', err);
         process.exit(1);
