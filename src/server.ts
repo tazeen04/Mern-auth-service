1aaf965458dbf53c9f3fd57a13c5174571febc8e
tazeen04 <atazeenm@gmail.com>
2025-04-25 21:44:01 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
 import { Config } from './config';
+import app from './app';
 
-console.log(Config.PORT);
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => console.log(`Listening on pory ${PORT}`));
+    } catch (err) {
+        console.error('Error starting server:', err);
+        process.exit(1);
+    }
+};
+
+startServer();
