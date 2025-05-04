a0c50c1eab2b77c7a3f29c96b7be21da0afdc3e8
tazeen04 <atazeenm@gmail.com>
2025-05-04 09:57:59 +0530

    test firstName, lastName & password is required

--- src/validators/resgister-validator.ts
+++ src/validators/resgister-validator.ts
@@ -6,6 +6,25 @@ export default checkSchema({
         notEmpty: true,
         trim: true,
     },
+    firstName: {
+        errorMessage: 'First name is required',
+        notEmpty: true,
+        trim: true,
+    },
+    lastName: {
+        errorMessage: 'Last name is required',
+        notEmpty: true,
+        trim: true,
+    },
+    password: {
+        errorMessage: 'Password is required',
+        notEmpty: true,
+        trim: true,
+        isLength: {
+            options: { min: 6 },
+            errorMessage: 'Password must be at least 6 characters long',
+        },
+    },
 });
 
 // export default [body("email").notEmpty().withMessage("Email is required")];
