a5d665be5886e4a2536efe3d845a39cd6ff03a66
tazeen04 <atazeenm@gmail.com>
2025-05-04 10:39:27 +0530

    test firstName

--- src/validators/resgister-validator.ts
+++ src/validators/resgister-validator.ts
@@ -6,6 +6,11 @@ export default checkSchema({
         notEmpty: true,
         trim: true,
     },
+    firstName: {
+        errorMessage: 'First name is required',
+        notEmpty: true,
+        trim: true,
+    },
     password: {
         errorMessage: 'Password is required',
         notEmpty: true,
