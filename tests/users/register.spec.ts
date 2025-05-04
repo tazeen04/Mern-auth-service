673c8b15a618bb947598cd7df5f37ef0f9394902
tazeen04 <atazeenm@gmail.com>
2025-05-04 10:42:30 +0530

    test password missing

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -223,6 +223,27 @@ describe('POST /auth/register', () => {
                 const users = await userRepository.find();
                 expect(users).toHaveLength(0);
             });
+            it('should return 400 status code if password is missing ', async () => {
+                // Arrange
+                const userData = {
+                    firstName: 'Ameena',
+                    lastName: 'tazeen',
+                    email: 'atazeenm@gmail.com',
+                    password: '',
+                };
+
+                // Act
+                const response = await request(app)
+                    .post('/auth/register')
+                    .send(userData);
+
+                // Assert
+                // console.log(response.body);
+                expect(response.statusCode).toBe(400);
+                const userRepository = connection.getRepository(User);
+                const users = await userRepository.find();
+                expect(users).toHaveLength(0);
+            });
         });
     });
 
