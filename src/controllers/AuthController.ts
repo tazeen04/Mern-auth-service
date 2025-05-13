85895d66eaf3ac436df8beb31223f8537876d447
tazeen04 <atazeenm@gmail.com>
2025-05-13 10:14:31 +0530

    refactor tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,15 +1,10 @@
-import fs from 'fs';
-import path from 'path';
 import { NextFunction, Response } from 'express';
 import { JwtPayload, sign } from 'jsonwebtoken';
 import { RegisterUserRequest } from '../types/index';
 import { UserService } from '../services/UserService';
 import { Logger } from 'winston';
 import { validationResult } from 'express-validator';
-import createHttpError from 'http-errors';
-import { Config } from '../config';
-import { AppDataSource } from '../config/data-source';
-import { RefreshToken } from '../entity/RefreshToken';
+import { TokenService } from '../services/TokenService';
 
 export class AuthController {
     // dependency injection
@@ -17,6 +12,7 @@ export class AuthController {
     constructor(
         private userService: UserService,
         private logger: Logger,
+        private tokenService: TokenService,
     ) {}
     // method
     async register(
@@ -48,46 +44,20 @@ export class AuthController {
             });
             this.logger.info('User has been registered', { id: user.id });
 
-            let privateKey: Buffer;
-            try {
-                privateKey = fs.readFileSync(
-                    path.join(__dirname, '../../certs/private.pem'),
-                );
-            } catch (err) {
-                const error = createHttpError(
-                    500,
-                    'Error while reading private key',
-                );
-                next(error);
-                return;
-            }
-
             const payload: JwtPayload = {
                 sub: String(user.id),
                 role: user.role,
             };
 
-            const accessToken = sign(payload, privateKey, {
-                algorithm: 'RS256',
-                expiresIn: '1h',
-                issuer: 'auth-service',
-            });
+            const accessToken = this.tokenService.generateAccesToken(payload);
 
             // Persist the refresh token
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365; //1y
-            const refreshTokenRepository =
-                AppDataSource.getRepository(RefreshToken);
+            const newRefreshToken =
+                await this.tokenService.persistRefreshToken(user);
 
-            const newRefreshToken = await refreshTokenRepository.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            });
-
-            const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
-                algorithm: 'HS256',
-                expiresIn: '1y',
-                issuer: 'auth-service',
-                jwtid: String(newRefreshToken.id),
+            const refreshToken = this.tokenService.generateRefreshToken({
+                ...payload,
+                id: String(newRefreshToken.id),
             });
 
             res.cookie('accessToken', accessToken, {
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,6 +5,8 @@ import { AppDataSource } from '../config/data-source';
 import { User } from '../entity/User';
 import logger from '../config/logger';
 import resgisterValidator from '../validators/resgister-validator';
+import { TokenService } from '../services/TokenService';
+import { RefreshToken } from '../entity/RefreshToken';
 
 // these are dependencies
 // for larger file we can use library like inversify
@@ -12,8 +14,10 @@ const router = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 // instanace for userService
 const userService = new UserService(userRepository);
+const refreshTokenRepository = AppDataSource.getRepository(RefreshToken);
+const tokenService = new TokenService(refreshTokenRepository);
 // instance of AuthController
-const authController = new AuthController(userService, logger);
+const authController = new AuthController(userService, logger, tokenService);
 
 router.post(
     '/register',
