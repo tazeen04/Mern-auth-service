137be38e7eba50da75cec08dfb24fcc7ddd1b926
tazeen04 <atazeenm@gmail.com>
2025-05-14 09:19:29 +0530

    change status code of login endpoint

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -161,7 +161,7 @@ export class AuthController {
             });
 
             this.logger.info('User has looged in', { id: user.id });
-            res.status(201).json({ id: user.id });
+            res.json({ id: user.id });
         } catch (err) {
             next(err);
             return;
