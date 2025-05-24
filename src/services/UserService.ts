3e356e4077c1aa897c4701c093bb10c2e1b3ee4f
tazeen04 <atazeenm@gmail.com>
2025-05-24 20:14:47 +0530

    update user

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,6 +1,6 @@
 import { Repository } from 'typeorm';
 import { User } from '../entity/User';
-import { UserData } from '../types';
+import { LimitedUserData, UserData } from '../types';
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 
@@ -57,5 +57,23 @@ export class UserService {
             },
         });
         return user;
+    }
+    async update(
+        userId: number,
+        { firstName, lastName, role }: LimitedUserData,
+    ) {
+        try {
+            return await this.userRepository.update(userId, {
+                firstName,
+                lastName,
+                role,
+            });
+        } catch (err) {
+            const error = createHttpError(
+                500,
+                'Failed to update the user in the database',
+            );
+            throw error;
+        }
     }
 }
